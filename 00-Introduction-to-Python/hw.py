f2e82b3504d9a9bc978bbe550220690c144c1357
tanya525625 <tanya_postcrossing@mail.ru>
2019-05-18 21:05:24 +0300

    solution added

--- 00-Introduction-to-Python/hw.py
+++ 00-Introduction-to-Python/hw.py
@@ -9,7 +9,28 @@
 
 def is_permutation(a: str, b: str) -> bool:
     # Нужно проверить, являются ли строчки 'a' и 'b' перестановками
+    if len(a) != len(b):
+        return False
+    else:
+        dict_a = dict.fromkeys(a, 0)
+        dict_b = dict.fromkeys(b, 0)
 
+        for i in a:
+            if i in dict.keys(dict_b):
+                dict_a[i] += 1
+            else:
+                return False
+        for i in b:
+            if i in dict.keys(dict_a):
+                dict_b[i] += 1
+            else:
+                return False
+        
+        for i in a:
+            if dict_a[i] != dict_b[i]:
+                return False
+        
+    return True
 
 assert is_permutation('baba', 'abab')
 assert is_permutation('abbba', 'abab')
