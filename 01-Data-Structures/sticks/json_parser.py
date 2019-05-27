c3f0bbd751f0c7380645a9a44d93b352856002c6
tanya525625 <tanya_postcrossing@mail.ru>
2019-05-27 23:30:38 +0300

    Update

--- 01-Data-Structures/sticks/json_parser.py
+++ 01-Data-Structures/sticks/json_parser.py
@@ -1,3 +1,4 @@
+import numpy as np
 import re
 
 def read_file(file_name):
@@ -16,23 +17,22 @@ def json_parser(json_file):
     
     return obj
     
-
 def get_dict(curr_str):
     keys = []
     values = []
     for curr_pair in re.finditer(r'"\w+": \w+', curr_str):
-        curr_pair = curr_pair[0].split(':')
+        curr_pair = curr_pair[0].split(': ')
         keys.append(curr_pair[0].replace('"', ''))
         values.append(curr_pair[1].replace('"', ''))
 
     for curr_pair in re.finditer(r'"\w+": "(.*?)"', curr_str):
-        curr_pair = curr_pair[0].split(':')
+        curr_pair = curr_pair[0].split(': ')
         keys.append(curr_pair[0].replace('"', ''))
-        values.append(curr_pair[1].replace('"', ''))
+        values.append(curr_pair[1].replace('"', '').replace("\\\\", "\\"))
     obj = dict(zip(tuple(keys), tuple(values)))
     
     for key in obj.keys():
-        if obj[key] == ' null':
+        if obj[key] == 'null':
             obj[key] = None
 
     return obj
@@ -44,58 +44,165 @@ def merge_files(f1, f2):
         if curr_obj not in f1:
             res.append(curr_obj)
 
-    res = sorted(res, key = lambda x: x['title'])
+    res = sorted(res, key = lambda x: x['variety']if x['variety'] is not None else 'z')
     res = sorted(res, key = lambda x: int(x['price']) if x['price'] is not None else 0)
     res.reverse()
 
     return res
 
-def save_json(obj):
-    output_file =  open('./files/winedata_full.json', 'w')
-    str_obj = str(obj).replace("' ",'"')
-    str_obj = str(obj).replace("'",'"')
-    #print(str_obj)
-    count = 0
-    count = 0
-    quotes_count = 0
+def write_json(obj):
+    output_file =  open('./files/winedata_full.json', 'w', encoding="utf-8")
 
-    for curr_char in str_obj:
-        if curr_char == '[' or curr_char == ']' or curr_char == '{' or curr_char == '}':
-            if curr_char == '[':
-                if count > 0:
-                    for i in range(count):
-                        output_file.write('\t')
-                output_file.write(f'{curr_char}\n')
-                count += 1
-            if curr_char == ']':
-                count -= 1
-                output_file.write(f'\n{curr_char}')
-            if curr_char == '{':
-                if count > 0:
-                    for i in range(count):
-                        output_file.write('\t')
-                count += 1
-                output_file.write(f'{curr_char}\n')
-            if curr_char == '}':
-                count += 1
-                output_file.write(f'\n{curr_char}')
-        else:
-            if curr_char == '"':
-                if quotes_count == 0:
-                    output_file.write('\t')
-                quotes_count += 1
-            elif curr_char == ',' and quotes_count % 2 == 0:
-                output_file.write(',\n')
+    curr_obj_ind = 0
+    if len(obj) > 1:
+        output_file.write('[\n')
+    for i in range(len(obj)):
+        curr_key_ind  = 0
+        output_file.write('\t{\n')
+        for key in obj[i].keys():
+            if obj[i][key] == None:
+                output_file.write(f'\t\t"{key}": null')
             else:
-                output_file.write(curr_char)
+                #[1:]
+                value = str(obj[i][key]).replace("\\\\", "\\")
+                output_file.write(f'\t\t"{key}": "{value}"')
+            if curr_key_ind != (len(obj[i].keys()) - 1):
+                output_file.write(',')
+            curr_key_ind += 1
+            output_file.write('\n')
+        if curr_obj_ind != len(obj) - 1:
+            output_file.write('\t},\n')
+        else:
+            output_file.write('\t}\n')
+        curr_obj_ind += 1
+    output_file.write('\n]')
             
     output_file.close()
     
 
-json_file =  open('./files/winedata_1.json', 'r')
+def find_options(obj):
+    output_file =  open('./files/markdown_file.txt', 'w', encoding="utf-8")
+    investigated_wines_list = ['Gewucrztraminer', 'Riesling', 'Merlot', 
+                          'Madeira Blend', 'Tempranillo', 'Red Blend']
+    investigated_obj = {
+        'Gewucrztraminer': [],
+        'Riesling': [],
+        'Merlot': [],
+        'Madeira Blend': [],
+        'Tempranillo': [],
+        'Red Blend': []
+    }
+
+
+    # options['avg_price', 'min_price', 'max_price', 'most_active_commentator', 'com_reg']
+    # wine_options = dict.fromkeys(investigated_wines_list)
+    # for i in range(len(obj)):
+    #     output_file.write(str(obj[i]['variety']))
+    #investigated_wines = []    
+    price = []
+    region = []
+    country = []
+    points = []    
+    taster_name = []
+    most_exp_wine = [] 
+    cheap_wine = []        
+    highest_score = []    
+    lowest_score = []
+    most_expensive_coutry = []
+    cheapest_coutry = []
+    most_rated_country = []
+    underrated_country = []
+    most_active_commentator = []
+
+    countries = {}
+    opt = {
+        'prices': [],
+        'scores': []
+    }
+
+    for j in range(len(investigated_wines_list)):
+        for i in range(len(obj)):
+            if obj[i]['variety'] == investigated_wines_list[j]:
+                investigated_obj[investigated_wines_list[j]].append(obj[i])
+    
+    for wine in investigated_obj.keys():
+        price.clear()
+        region.clear()
+        country.clear()
+        points.clear()
+        taster_name.clear()
+        most_exp_wine.clear()
+        cheap_wine.clear()
+        highest_score.clear()
+        lowest_score.clear()
+        most_expensive_coutry.clear()
+        cheapest_coutry.clear()
+        most_rated_country.clear()
+        underrated_country.clear()
+        most_active_commentator.clear()
+
+        for i in range(len(investigated_obj[wine])):
+            curr_wine = investigated_obj[wine][i]
+            if curr_wine['country'] != None:
+                countries.update(curr_wine['country']: opt)
+            if curr_wine['price'] != None:
+                price.append(int(curr_wine['price']))
+                countries[curr_wine['country']]['prices'].append(curr_wine['price'])
+            if curr_wine['points'] != None:
+                points.append(int(curr_wine['points']))
+                countries[curr_wine['country']]['scores'].append(curr_wine['points'])
+            if curr_wine['region_1'] != None:
+                region.append(curr_wine['region_1'])
+            if curr_wine['region_2'] != None:
+                region.append(curr_wine['region_2'])
+            if curr_wine['country'] != None:
+                country.append(curr_wine['country'])
+            if curr_wine['taster_name'] != None:
+                taster_name.append(curr_wine['taster_name'])
+            
+
+        avg_price = np.mean(price)
+        max_price = np.amax(price)
+        min_price = np.amin(price)
+        com_reg = max(set(region), key=region.count)
+        com_country = max(set(country), key=country.count)
+        avg_score = np.mean(points)
+        max_score = np.amax(points)
+        min_score = np.amin(points)
+        act_com = max(set(taster_name), key=taster_name.count)
+
+        output_file.write(f'{wine}\n')
+        output_file.write(f'Average price is {avg_price}\n')
+        output_file.write(f'Max price is {max_price}\n')
+        output_file.write(f'Min price is {min_price}\n')
+        output_file.write(f'Most common region is {com_reg}\n')
+        output_file.write(f'Most common country is {com_country}\n')
+        output_file.write(f'Average score is {avg_score}\n')
+        output_file.write('=' * 20 + '\n')
+
+        for i in price:
+            if i == max_price:
+                most_exp_wine.append(curr_wine['title'])
+            if i == min_price:
+                cheap_wine.append(curr_wine['title'])
+        for i in points:
+            if i == max_score:
+                most_exp_wine.append(curr_wine['title'])
+            if i == min_score:
+                cheap_wine.append(curr_wine['title'])
+        for i in taster_name:
+            if i == act_com:
+                most_active_commentator.append(curr_wine['taster_name'])
+        for i in countries:
+            if
+
+    #print(np.mean(price))
+    output_file.close()
+
+json_file =  open('./files/winedata_1.json', 'r', encoding="utf-8")
 file1 = read_file(json_file)
 json_file.close()
-json_file =  open('./files/winedata_2.json', 'r')
+json_file =  open('./files/winedata_2.json', 'r', encoding="utf-8")
 file2 = read_file(json_file)
 json_file.close()
 
@@ -104,4 +211,6 @@ winedata_2 = json_parser(file2)
 
 full_winedata = merge_files(winedata_1, winedata_2)
 
-save_json(full_winedata)
+#write_json(full_winedata)
+
+find_options(full_winedata)
