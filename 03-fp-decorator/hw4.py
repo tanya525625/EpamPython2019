2f83efd6b4d2f10b5c82947d2b1b2cd12462476b
tanya525625 <tanya_postcrossing@mail.ru>
2019-06-02 18:45:27 +0300

    done hw4

---
+++ 03-fp-decorator/hw4.py
@@ -0,0 +1,40 @@
+# Переписать функцию make_cache, которая сохраняет
+# результаты предыдущих вызовов оборачиваемой функции,
+# таким образом, чтобы она сохраняла результаты в своем
+# хранилищe на определенное время, которое передается
+# параметром (аргументом) в декоратор.
+
+
+# Плюс придумать некоторый полезный юзкейс и заимплементировать функцию slow_function
+import time
+
+def make_cache(bound):
+    def modify_func(func):
+        def new_function(*args, **kwargs):
+            cache = []
+            cache.append(func(*args, **kwargs))
+            for i in range(len(cache)):
+                print(time.time() - cache[i]['time'])
+                if (time.time() - cache[i]['time']) > bound:
+                    cache.pop(0)
+            return cache
+        return new_function
+    return modify_func
+
+@make_cache(30)
+def slow_function(type_of_payment, customer_name):
+    """
+    A func for storaging information about purchase
+    in the shop. If the warranty expires, the information
+    can be deleted.
+    """
+    product = {
+        'type_of_payment': type_of_payment,
+        'customer': customer_name, 
+        'time': time.time()
+        }
+    return product
+
+
+slow_function('card', 'Ivanov')
+print(slow_function('card', 'Ivanov'))
