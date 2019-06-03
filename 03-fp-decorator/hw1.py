47b70754b2eed01df54825fd7a58654a4e6ead81
tanya525625 <tanya_postcrossing@mail.ru>
2019-06-03 12:53:18 +0300

    Update

--- 03-fp-decorator/hw1.py
+++ 03-fp-decorator/hw1.py
@@ -2,13 +2,16 @@
 def find_sum(bound):
     return sum(filter(lambda x: x % 3 == 0 or x % 5 == 0, range(bound)))
 
-# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+# Find the difference between the sum of the squares of the first 
+# one hundred natural numbers and the square of the sum.
 def sum_squares(bound):
     return sum(range(bound+1))**2 - sum(x**2 for x in range(bound+1))
 
-#Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+#Find the sum of all the numbers that can be written 
+# as the sum of fifth powers of their digits.
 def sum_of_fifth_pow_of_numb(bound):
-    return sum(s for s in range(bound) if s == sum(int(x)**5 for x in list(str(s)))) - 1
+    return sum(s for s in range(bound) if s == sum(int(x)**5 
+                                 for x in list(str(s)))) - 1    
 
 print(find_sum(1000))
 print(sum_of_fifth_pow_of_numb(500000))
