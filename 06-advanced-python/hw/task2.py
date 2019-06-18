357a8c6c7d99f6d182f69ca0964f21289507914d
tanya525625 <tanya_postcrossing@mail.ru>
2019-06-18 10:21:32 +0300

    Done task2

--- 06-advanced-python/hw/task2.py
+++ 06-advanced-python/hw/task2.py
@@ -6,20 +6,30 @@ E - dict(<V> : [<V>, <V>, ...])
 Сделать так, чтобы по графу можно было итерироваться(обходом в ширину)
 
 """
+from queue import Queue
 
 
 class Graph:
     def __init__(self, E):
         self.E = E
-
+        
     def __iter__(self):
         self.nodes_count = len(self.E.keys())
-
+        self.processed = [tuple(self.E.keys())[0]]
+        self.nodes_queue = Queue()
+        self.nodes_queue.put(tuple(self.E.keys())[0])
         return self
 
-     def __next__(self):
-
-
+    def __next__(self):
+        while not self.nodes_queue.empty():
+            curr_node = self.nodes_queue.get()
+            for neighbour_node in self.E[curr_node]:
+                if neighbour_node not in self.processed:
+                    self.nodes_queue.put(neighbour_node)
+                    self.processed.append(neighbour_node)
+            return curr_node        
+        raise StopIteration
+        
 
 E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': [], 'D': ['A']}
 graph = Graph(E)
