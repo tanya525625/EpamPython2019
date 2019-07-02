4f0947d62c44ed9412fe3e6b035c213f262e1f42
tanya525625 <tanya_postcrossing@mail.ru>
2019-07-02 11:07:22 +0300

    done decorator

--- 10-Design_Patterns/hw/1-Decorator/decorator.py
+++ 10-Design_Patterns/hw/1-Decorator/decorator.py
@@ -9,9 +9,32 @@ class Component:
         raise NotImplementedError("Override get_cost method")
 
 
+class BaseDecorator(Component):
+    def __init__(self, component):
+        self._component = component
+
+    def get_cost(self):
+        return self._component.get_cost()
+
+
 class BaseCoffe(Component):
     def get_cost(self):
         return 90
+
+
+class Whip(BaseDecorator):
+    def get_cost(self):
+        return self._component.get_cost() + 20
+
+
+class Marshmallow(BaseDecorator):
+    def get_cost(self):
+        return self._component.get_cost() + 30
+
+
+class Syrup(BaseDecorator):
+    def get_cost(self):
+        return self._component.get_cost() + 15
 
 
 if __name__ == "__main__":
